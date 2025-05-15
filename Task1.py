5e1b264cdd30596dc59fd57c7270165f0e841452
Muskan <muskanyav06132013@gmail.com>
2025-05-15 12:19:24 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,1 +1,14 @@
-
+#Input the two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#Addition
+print("\nAddition: ",num1+num2)
+#Subtraction
+print("Subtraction: ",num1-num2)
+#Multiplication 
+print("Multiplication: ",num1*num2)
+# Division and handling the exception of zero
+if num2 != 0:
+    print("Division: ", num1 / num2)
+else:
+    print("Division: Cannot divide by zero.")
